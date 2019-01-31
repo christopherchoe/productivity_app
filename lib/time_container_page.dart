4a5347c66382100bcbe23c7ea940a30688fa4223
christopherchoe <chchchoe@gmail.com>
2019-01-30 20:50:08 -0800

    updated to have bottom navigation bar with color change

--- lib/time_container_page.dart
+++ lib/time_container_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomTimeContainer extends StatelessWidget {
-  CustomTimeContainer({this.label, this.value});
+  CustomTimeContainer({this.label, this.value, this.color});
 
   final String label;
   final String value;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 5),
-      padding: EdgeInsets.all(25),
+      padding: EdgeInsets.all(35),
       decoration: new BoxDecoration(
         borderRadius: new BorderRadius.circular(15),
-        color: Colors.black38,
+        color: color
       ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
--- lib/timer_page.dart
+++ lib/timer_page.dart
@@ -10,12 +10,33 @@ class TimerCustom extends StatefulWidget {
 }
 
 class TimerCustomState extends State<TimerCustom> {
+
   static const duration = const Duration(seconds:1);
 
   int secondsPassed = 0;
   bool isActive = false;
 
   Timer timer;
+
+  int navigationIndex = 0;
+
+  final List<Color> navigationColors = [
+    Colors.black54,
+    Colors.redAccent,
+    Colors.blueAccent
+  ];
+
+  final List<int> navigationTimes = [
+    300,
+    1500,
+    1200
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      navigationIndex = index;
+    });
+  }
 
   void incrementSecond() {
     if (isActive) {
@@ -44,6 +65,7 @@ class TimerCustomState extends State<TimerCustom> {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Productivity Push'),
+          backgroundColor: navigationColors[navigationIndex],
         ),
         body: Center(
           child: Column(
@@ -53,13 +75,16 @@ class TimerCustomState extends State<TimerCustom> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   CustomTimeContainer(
-                      label: 'Minutes', value: minutes.toString()),
+                    label: 'Minutes', value: minutes.toString(),
+                      color: navigationColors[navigationIndex]),
                   CustomTimeContainer(
-                      label: 'Seconds', value: seconds.toString()),
+                    label: 'Seconds', value: seconds.toString(),
+                    color: navigationColors[navigationIndex]),
                 ],
               ),
               Container(
                 margin: EdgeInsets.only(top:25),
+                color: navigationColors[navigationIndex],
                 child: RaisedButton(
                   child: Text(isActive ? 'PAUSE' : 'START'),
                   onPressed: (){
@@ -71,6 +96,24 @@ class TimerCustomState extends State<TimerCustom> {
               ),
             ],
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: navigationIndex, // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.filter_5),
+              title: new Text('Just Five Minutes'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.timer),
+              title: new Text('Traditional Pomodoro'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.laptop),
+                title: Text('20/20/20')
+            )
+          ],
         ),
       ),
     );
