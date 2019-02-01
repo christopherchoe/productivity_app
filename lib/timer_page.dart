e3678b515023dfcff8c72b3edc737598f434e1c4
christopherchoe <chchchoe@gmail.com>
2019-02-01 06:48:47 -0800

    working barebones prototype

--- lib/timer_page.dart
+++ lib/timer_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'time_container_page.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class TimerCustom extends StatefulWidget {
   @override
@@ -18,7 +19,9 @@ class TimerCustomState extends State<TimerCustom> {
 
   Timer timer;
 
-  int navigationIndex = 0;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  int navigationIndex =0;
 
   final List<Color> navigationColors = [
     Colors.black54,
@@ -26,11 +29,22 @@ class TimerCustomState extends State<TimerCustom> {
     Colors.blueAccent
   ];
 
-  final List<int> navigationTimes = [
-    300,
-    1500,
-    1200
+  final List<String> timerMessages = [
+    "Five minutes done, keep going!",
+    "Pomodoro done, take a five minute break!",
+    "Look twenty feet away for twenty seconds!",
+    "Just Five Minutes",
+    "Pomodoro",
+    "20/20/20"
   ];
+
+  final List<int> navigationTimes = [
+    3,
+    5,
+    12
+  ];
+
+  int workTimer = 0;
 
   void onTabTapped(int index) {
     setState(() {
@@ -42,16 +56,62 @@ class TimerCustomState extends State<TimerCustom> {
     if (isActive) {
       setState(() {
         secondsPassed = secondsPassed + 1;
+        if (secondsPassed >= workTimer)
+          {
+            showNotificationWithDefaultSound();
+            isActive = false;
+            secondsPassed = 0;
+          }
       });
     }
   }
 
+  Future showNotificationWithDefaultSound() async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      timerMessages[navigationIndex + 3],
+      timerMessages[navigationIndex],
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  Future onSelectNotification(String payload) async {
+    showDialog(
+      context: context,
+      builder: (_) => new AlertDialog(
+          title: const Text("Here is your payload"),
+          content: new Text("Payload: $payload"),
+      ),
+    );
+  }
+
   @override
   void initState() {
+
     timer = Timer.periodic(duration, (Timer t) {
       incrementSecond();
     });
     super.initState();
+
+    var initializationSettingsAndroid =
+    new AndroidInitializationSettings('app_icon');
+
+    var initializationSettingsIOS = new IOSInitializationSettings();
+
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
   }
 
   @override
@@ -59,6 +119,8 @@ class TimerCustomState extends State<TimerCustom> {
 
     int seconds = secondsPassed % 60;
     int minutes = secondsPassed ~/ 60;
+
+    workTimer = navigationTimes[navigationIndex];
 
     return MaterialApp(
       title: 'Productivity Push',
@@ -78,7 +140,8 @@ class TimerCustomState extends State<TimerCustom> {
                   child: Text('RESET'),
                   onPressed: (){
                     setState(() {
-                      isActive = !isActive;
+                      secondsPassed = 0;
+                      isActive = false;
                     });
                   },
                 ),
